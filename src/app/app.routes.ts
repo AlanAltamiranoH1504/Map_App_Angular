eab9a2176a034cd061a92609009cf64d591a4e52
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2026-04-20 17:21:42 -0600

    feat: add base project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { FullscreenMapPage } from './pages/fullscreen-map-page/fullscreen-map-page';
+import { MarkersPage } from './pages/markers-page/markers-page';
+import { HousesPage } from './pages/houses-page/houses-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "fullscreen", component: FullscreenMapPage, title: "FullScreen Map"},
+    {path: "markers", component: MarkersPage, title: "Marcadores"},
+    {path: "house", component: HousesPage, title: "Casas - Propiedades"},
+    {path: "**", redirectTo: "fullscreen"}
+];
